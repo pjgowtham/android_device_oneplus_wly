69a28435fc90cfcb0767dc612d0e6f472938e9ae
pjgowtham <pjgowtham@gmail.com>
2023-05-24 22:13:40 +0530

    wly: Initial tree from NE2213_14.0.0.402(EX01) and add prebuilt DTB/O
    Change-Id: I14581196331c7ba714c352c3fbca330683eedabf
    Co-authored-by: Zinadin Zidan <zidan.roking@gmail.com>
    Co-authored-by: Albert Tang <tangalbert919@yahoo.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,11 @@ DEVICE_PATH := device/oneplus/wly
 # Display
 TARGET_SCREEN_DENSITY := 450
 
+# DTB/O
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilts/dtb.img
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
